6a3620aa3f4c4c2b886cf6ce87176e82ee14167d
Logan Gore <logangster@Logans-MacBook-Pro.local>
2017-04-19 21:17:26 -0600

    Added onboarding for xbox live gamertags

--- src/main/java/com/UnitedWeGame/models/GamerIdentifier.java
+++ src/main/java/com/UnitedWeGame/models/GamerIdentifier.java
@@ -18,6 +18,7 @@ public class GamerIdentifier {
 	private String identifier;
 	private String platform;
 	
+	
 	public String getIdentifier() {
 		return identifier;
 	}
@@ -33,4 +34,5 @@ public class GamerIdentifier {
 	public Long getId() {
 		return id;
 	}
+	
 }
