f59fc0fa7d44199bda7fd7e7a7aff977973f509d
Logan Gore <logangster@Logans-MacBook-Pro.local>
2017-04-24 21:58:53 -0600

    Fixed time zone again.. let's hope this works

--- src/main/java/com/UnitedWeGame/services/UserService.java
+++ src/main/java/com/UnitedWeGame/services/UserService.java
@@ -196,7 +196,7 @@ public class UserService implements UserDetailsService {
 		Date currentDate = new Date();
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(currentDate);
-		cal.add(Calendar.HOUR, -5);
+		cal.add(Calendar.HOUR, -7);
 		Criteria query = session.createCriteria(OnlineFeed.class, "onlineFeed")
 				.createAlias("onlineFeed.user", "users")
 				.add(Restrictions.eq("users.id", userId))
@@ -223,7 +223,7 @@ public class UserService implements UserDetailsService {
 		Date currentDate = new Date();
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(currentDate);
-		cal.add(Calendar.HOUR, -5);
+		cal.add(Calendar.HOUR, -7);
 		Criteria query = session.createCriteria(OnlineFeed.class, "onlineFeed")
 				.createAlias("onlineFeed.user", "users")
 				.add(Restrictions.eq("users.id", userId))
