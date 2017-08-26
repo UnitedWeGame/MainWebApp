7193fae3b7d854e240143192a568c9ddd47c59b5
Logan Gore <logangster@Logans-MacBook-Pro.local>
2017-08-26 16:07:16 -0600

    Added a redirect to users center hub if the user is logged in and made sure image was stretching properly on landing page

--- src/main/java/com/UnitedWeGame/controllers/HomeController.java
+++ src/main/java/com/UnitedWeGame/controllers/HomeController.java
@@ -1,13 +1,25 @@
 package com.UnitedWeGame.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.UnitedWeGame.services.UserService;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	UserService userService;
+	
 	@RequestMapping("/")
-	public String index() {
-		return "index";
+	// Principal returns null if user isn't logged in
+	public String index(Principal user) {
+		if (user == null)
+			return "index";
+		else
+			return "redirect:/users";
 	}
 }
