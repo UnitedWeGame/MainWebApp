6105af85dcfca6c10186ea5124f8d8bee8b498cf
Logan Gore <logangster@Logans-MacBook-Pro.local>
2017-09-10 16:21:11 -0600

    Added userReview and friendsRatings to game json

---
+++ src/main/java/com/UnitedWeGame/models/GameRatingSerializer.java
@@ -0,0 +1,31 @@
+package com.UnitedWeGame.models;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class GameRatingSerializer extends StdSerializer<GameRating> {
+    
+    public GameRatingSerializer() {
+        this(null);
+    }
+   
+    public GameRatingSerializer(Class<GameRating> t) {
+        super(t);
+    }
+ 
+    @Override
+    public void serialize(
+      GameRating value, JsonGenerator jgen, SerializerProvider provider) 
+      throws IOException, JsonProcessingException {
+  
+        jgen.writeStartObject();
+        jgen.writeNumberField("id", value.getId());
+        jgen.writeStringField("username", value.getUser().getUsername());
+        jgen.writeNumberField("rating", value.getRating());
+        jgen.writeEndObject();
+    }
+}
