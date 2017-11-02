1540cf482e668f05a72d69361a850e2db4edde66
heidariank <heidariank@gmail.com>
2017-11-02 10:16:44 -0600

    Group Settings can be edited. Data is currently just dummy data.

---
+++ src/main/js/stores/GroupStore.js
@@ -0,0 +1,39 @@
+import { EventEmitter } from "events";
+import dispatcher from "../dispatcher";
+
+class GroupStore extends EventEmitter{
+  constructor(){
+    super();
+    this.group = [];
+  }
+
+  setGroup(group){
+    this.group = group;
+    this.emit("change");
+  }
+
+  getName(){
+    return this.group.groupName;
+  }
+
+  getDesc(){
+    return this.group.description;
+  }
+
+  getCoverPhoto(){
+    return this.group.coverPhoto;
+  }
+
+
+  handleActions(action){
+    switch (action.type) {
+      case "GET_GROUP_DATA": {
+        this.setGroup(action.group);
+        break;
+      }
+    }
+  }
+}
+const groupStore = new GroupStore();
+dispatcher.register(groupStore.handleActions.bind(groupStore));
+export default groupStore;
