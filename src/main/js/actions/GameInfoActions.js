ec6ae51eaae67c791fdd0559ef3a548391ba4177
Jackson Murphy <jackson@Jacksons-MacBook-Air.local>
2017-11-09 17:10:53 -0700

    fixed json post data. posting new game review now works

--- src/main/js/actions/GameInfoActions.js
+++ src/main/js/actions/GameInfoActions.js
@@ -3,28 +3,30 @@ import dispatcher from "../dispatcher";
 /* Posts a new game rating (1-5) to our server */
 export function postRating(id, rating){
     var data = {"rating": rating};
-    $.ajax({
-       url: "/api/games/" + id + "/addRating",
-       type:"POST",
-       data: JSON.stringify({data}),
-       contentType:"application/json; charset=utf-8",
-       dataType:"json",
-       success: function(response ){
-         console.log("Response after posting new game rating:" + response);
-       }
-    });
+    // $.ajax({
+    //    url: "/api/games/" + id + "/addRating",
+    //    type:"POST",
+    //    data: JSON.stringify({data}),
+    //    contentType:"application/json; charset=utf-8",
+    //    dataType:"json",
+    //    success: function(response ){
+    //      console.log("Response after posting new game rating:" + response);
+    //    }
+    // });
 }
 
 /* Posts a game review to our server */
 export function postGameReview(id, reviewTitle, review, rating){
-    console.log("posting new game review: " + review)
-    console.log("rating: " + rating)
-    var data = {"rating": rating, "reviewTitle": reviewTitle, "review": review};
-    console.log("after json: " + JSON.stringify({data}))
     $.ajax({
        url: "/api/games/" + id + "/addRating",
        type:"POST",
-       data: JSON.stringify({data}),
+       data: JSON.stringify(
+         {
+           "rating": rating,
+           "reviewTitle": reviewTitle,
+           "review": review
+          }
+       ),
        contentType:"application/json; charset=utf-8",
        dataType:"json",
        success: function(response ){
